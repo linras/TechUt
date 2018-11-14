e411ab51d26f658250551ce8517112b74209a23d
Linras <linras@wp.pl>
2018-11-14 16:31:36 +0100

    new project zad03

---
+++ zad03/README.md
@@ -0,0 +1,6 @@
+Zadanie z transakcjami
+
+Aby zainicjować działanie programu należy:
+1. Uruchomić skrypt runSQLDBServer.sh
+2. Uruchomić program Main
+3. Następnie w innym terminalu uruchomić runSQLDBClient.sh
---
+++ zad03/src/main/java/aformela/zad02/test/BirdServiceTest.java
@@ -0,0 +1,30 @@
+package aformela.zad02.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import aformela.zad02.domain.Bird;
+import aformela.zad02.service.BirdService;
+
+public class BirdServiceTest {
+	BirdService bs = new BirdService();
+	final private String BIRD1_NAME = "Piszczel";
+	
+	@Test
+	public void addBirdCheck() throws SQLException {
+		
+		assertNotNull(bs);
+		
+		Bird bird1 = new Bird(BIRD1_NAME, "2008-01-11", true, 1.7, 2);
+		
+		//bs.addBird(bird1);
+		//List<Bird> retrivedBirds = bs.getAllBirds();
+		//Bird retrivedBird = retrivedBirds.get(0);
+		Bird retrivedBird = bird1;
+		assertEquals(BIRD1_NAME, retrivedBird.getName());
+		//assertEquals(DOG_COS, retrivedBird.getCos());
+	}
+}
