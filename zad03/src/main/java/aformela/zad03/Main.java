8aa516084887f37a3185eb6a98fb84c8f3ffb452
Linras <linras@wp.pl>
2019-01-23 13:57:39 +0100

    zad03 final

--- zad03/src/main/java/aformela/zad03/Main.java
+++ zad03/src/main/java/aformela/zad03/Main.java
@@ -16,29 +16,33 @@ public class Main {
         List<Bird> birds = new ArrayList<>();
         
         
-        Bird bird1 = new Bird("Paprotka:", "1997-02-11", true, 1.2, 5);	// 
-        Bird bird2 = new Bird("Krakers:", "2017-03-02", false, 2.2, 7);	
-        Bird bird3 = new Bird("Piszczel:", "2008-01-11", true, 1.7, 2);	
+        Bird bird1 = new Bird("Paprotka", "1997-02-11", true, 1.2, 5);	// 
+        Bird bird2 = new Bird("Krakers", "2017-03-02", false, 2.2, 7);	
+        Bird bird3 = new Bird("Piszczel", "2008-01-11", true, 1.7, 2);	
         /*birdService.addBird(bird1);
         birdService.addBird(bird2);
-        birdService.addBird(bird3);
-        birdService.deleteBird(bird1);*/
+        birdService.addBird(bird3);*/
+        birdService.deleteAllBirds();
+        /*birdService.deleteBird(bird1);
+        birdService.deleteBird(bird2);
+        birdService.deleteBird(bird3);*/
         birds.add(bird1);
         birds.add(bird2);
         birds.add(bird3);
         
         System.out.println(birdService.addAllBirds(birds));
-
-        Bird bird4= new Bird("Padzio:", "1997-02-11", true, 1.2, 5);	// 
-        Bird bird5 = new Bird("Krowa:", "2017-03-02", false, 2.2, 7);	
-        Bird bird6 = new Bird("Piszcz:", "2008-01-11", true, 1.7, 2);	
+        System.out.println(birdService.findByName("Piszczel").toString());
+        /*
+        Bird bird4= new Bird("Panaceum:", "1997-02-11", false, 1.2, 5);	// 
+        Bird bird5 = new Bird("Krowa:", "2017-03-02", true, 2.2, 7);	
+        Bird bird6 = new Bird("Bambol:", "2008-01-11", false, 1.7, 2);	
         
         List<Bird> birds2 = new ArrayList<>();
         birds.add(bird4);
         birds.add(bird2);
         birds.add(bird5);
         
-        System.out.println(birdService.findByName("Piszczel").toString());
+        */
         //System.out.println(birdService.deleteBird(bird6));
         
 }
--- zad03/src/main/java/aformela/zad03/test/BirdServiceTest.java
+++ zad03/src/main/java/aformela/zad03/test/BirdServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import aformela.zad03.domain.Bird;
@@ -11,20 +13,72 @@ import aformela.zad03.service.BirdService;
 
 public class BirdServiceTest {
 	BirdService bs = new BirdService();
-	final private String BIRD1_NAME = "Piszczel";
+	private final String NAME_BIRD_1 = "Dryante";
+	private final String NAME_BIRD_2 = "Delain";
+	private final String NAME_BIRD_3 = "Danifae";
+	private final double WEIGHT_BIRD_1 = 12.5;
+	private final double WEIGHT_BIRD_2 = 10.0;
+	private final double WEIGHT_BIRD_3 = 8.5;
+	private final String DATE_BIRD_1 = "2017-07-07";
+	private final String DATE_BIRD_2 = "2016-06-06";
+	private final String DATE_BIRD_3 = "2015-05-05";
+	private final int COLORS_BIRD_1 = 5;
+	private final int COLORS_BIRD_2 = 7;
+	private final int COLORS_BIRD_3 = 4;
+	
+	/*@Test
+	public void addBirdTest() throws SQLException {
+		Bird b = new Bird(NAME_BIRD_1, DATE_BIRD_1, true, WEIGHT_BIRD_1, COLORS_BIRD_1);
+		bs.addBird(b);
+
+		Bird retrievedBird = bs.findByName(b.getName());
+		
+		assertEquals(NAME_BIRD_1, retrievedBird.getName());
+		assertEquals(DATE_BIRD_1, retrievedBird.getDateOfBirth());
+	}
 	
 	@Test
-	public void addBirdCheck() throws SQLException {
+	public void getAllBirdsTest() throws SQLException {
+		Bird b1 = new Bird(NAME_BIRD_1, DATE_BIRD_1, true, WEIGHT_BIRD_1, COLORS_BIRD_1);
+		Bird b2 = new Bird(NAME_BIRD_2, DATE_BIRD_2, true, WEIGHT_BIRD_2, COLORS_BIRD_2);
+		Bird b3 = new Bird(NAME_BIRD_3, DATE_BIRD_3, false, WEIGHT_BIRD_3, COLORS_BIRD_3);
+		bs.addBird(b1);
+		bs.addBird(b2);
+		bs.addBird(b3);
+
+		assertEquals(3, bs.getAllBirds().size());
+	}*/
+	
+	/*@Test
+	public void deleteBirdTest() throws SQLException {
+		Bird b = new Bird(NAME_BIRD_1, DATE_BIRD_1, true, WEIGHT_BIRD_1, COLORS_BIRD_1);
+		bs.addBird(b);
+
+		int count = bs.getAllBirds().size();
+		bs.deleteBird(b);
+
+		assertEquals(count-1, bs.getAllBirds().size());
+	}*/
+	
+	@Test
+	public void addAllBirdsTest() throws SQLException {
+		List<Bird> birds = new ArrayList<Bird>();
+		Bird b1 = new Bird(NAME_BIRD_1, DATE_BIRD_1, true, WEIGHT_BIRD_1, COLORS_BIRD_1);
+		Bird b2 = new Bird(NAME_BIRD_2, DATE_BIRD_2, true, WEIGHT_BIRD_2, COLORS_BIRD_2);
+		Bird b3 = new Bird(NAME_BIRD_3, DATE_BIRD_3, false, WEIGHT_BIRD_3, COLORS_BIRD_3);
 		
-		assertNotNull(bs);
+		birds.add(b1);
+		birds.add(b2);
+		birds.add(b3);
 		
-		Bird bird1 = new Bird(BIRD1_NAME, "2008-01-11", true, 1.7, 2);
-		
-		//bs.addBird(bird1);
-		//List<Bird> retrivedBirds = bs.getAllBirds();
-		//Bird retrivedBird = retrivedBirds.get(0);
-		Bird retrivedBird = bird1;
-		assertEquals(BIRD1_NAME, retrivedBird.getName());
-		//assertEquals(DOG_COS, retrivedBird.getCos());
+		bs.addAllBirds(birds);
+
+		assertEquals(3, bs.getAllBirds().size());
+	}
+	
+	@Test
+	public void deleteAllBirdsTest() throws SQLException {
+		bs.deleteAllBirds();
+		assertEquals(0, bs.getAllBirds().size());
 	}
 }
